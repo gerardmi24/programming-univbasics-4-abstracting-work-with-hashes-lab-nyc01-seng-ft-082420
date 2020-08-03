1393d3a2c209d53079bf1ee8417261eae6123cc5
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-08-03 22:41:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,8 +5,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  if {read_from_hash => key}
-    puts key
+  {read_from_hash => key}
 end
 
 def update_counting_hash(hash, key)
