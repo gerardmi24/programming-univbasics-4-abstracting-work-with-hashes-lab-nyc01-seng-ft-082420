3a8a057fe4aaa59f681119e5cd6232e2a3d3fcab
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-08-03 22:33:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  {:fav_food => "ribs"}
+my_hash_creator[:name]
 end
 
 def read_from_hash(hash, key)
